877cbea93f64d7ea4b29c11cc5303fb8c9b8d4c6
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-26 16:05:39 -0400

    commit tree 3b169091c36a0fdeb809b69c9fa69512b5b14175

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[$@!#%\*\(\)\}\{\^\]]/gim,"")
+  }
+  static titleize(string){
+    const nonCapWords = ["the","an","a","but","of","and","for","by","from","at"]
+    const wordArray = string.split(' ')
+    const titleArray = wordArray.map((word) => {
+      if(nonCapWords.includes(word)){
+        return word
+      }else{
+        return word[0].toUpperCase() + word.slice(1)
+      }
+    })
+    return titleArray.join(' ')[0].toUpperCase() + titleArray.join(' ').slice(1)
+
+  }
 }
+
